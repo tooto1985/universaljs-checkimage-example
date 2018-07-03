80c537bf2853520d2f838c8b3908095891274968
Tommy <tooto1985@gmail.com>
2018-07-03 17:10:35 +0800

    modify

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser')
 var fileUpload = require('express-fileupload')
 var sizeOf = require('image-size')
 
+var tools = require('./public/tools')
+
 var app = express()
 
 app.use(fileUpload({
@@ -20,21 +22,15 @@ app.post('/upload', function (req, res) {
   if (!(req.files.upf instanceof Array)) {
     req.files.upf = [req.files.upf]
   }
-  if (req.files.upf.length <= 3) {
-    if (req.files.upf.every(a => a.truncated === false)) {
-      if (req.files.upf.every(a => {
-        let size = sizeOf(a.data)
-        return size.width <= 30 && size.height <= 30
-      })) {
-        res.send('通過')
-      } else {
-        res.send('尺寸超過30x30像素')
-      }
-    } else {
-      res.send('檔案超過3kb')
-    }
-  } else {
-    res.send('超過3張照片')
+  var files = req.files.upf.map(a => {
+    let size = sizeOf(a.data)
+    return {width: size.width, height: size.height, size: a.data.length}
+  })
+  try {
+    tools.checkImage(files)
+    res.send('ok')
+  } catch (e) {
+    res.send(`error:${e.message}`)
   }
 })
 app.listen(process.env.PORT || 3000)
